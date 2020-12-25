597bb210163cffb94e75771d5afebd0722928853
Francisco Edilton <franciscoed@gmail.com>
2020-12-25 14:47:39 +0000

    check permissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if not os.path.isdir("/tmp/ta-lib"):
     # untar
     os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
     os.chdir("/tmp/ta-lib")
-    os.system("ls -la /app/")
+    os.system("ls -la /app/equity/")
     # build
     os.system("./configure --prefix=/home/appuser")
     os.system("make")
