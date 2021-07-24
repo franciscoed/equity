835a921b72670499b8854c9c14c299de0bede375
Francisco Edilton <franciscoed@gmail.com>
2021-07-24 14:07:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if not os.path.isdir("/tmp/ta-lib"):
     sys.stdout.flush()
 
 os.system("ls -la /home/appuser/lib")
+os.system("pip3 list")
 # add the library to our current environment
 print("Import ctypes")
 from ctypes import *
