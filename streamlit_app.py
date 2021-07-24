03943e8babc0eeb00120b57e05ce8f4c2575d2c4
Francisco Edilton <franciscoed@gmail.com>
2021-07-24 14:04:21 +0100

    ts

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,9 @@ if not os.path.isdir("/tmp/ta-lib"):
     # back to the cwd
     os.chdir(default_cwd)
     print(os.getcwd())
-    os.system("ls -la /home/appuser")
     sys.stdout.flush()
 
+os.system("ls -la /home/appuser")
 # add the library to our current environment
 print("Import ctypes")
 from ctypes import *
