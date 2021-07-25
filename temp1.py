9b986ec9e24322f3eba6d20952fabf5c987bcb78
Francisco Edilton <franciscoed@gmail.com>
2021-07-25 15:25:57 +0100

    Create temp1.py

---
+++ temp1.py
@@ -0,0 +1,10 @@
+print("Import ctypes")
+from ctypes import *
+
+print("CDLL") 
+lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
+print(str(lib))
+print("Importing talib")
+sys.stdout.flush()
+# import library
+import talib
